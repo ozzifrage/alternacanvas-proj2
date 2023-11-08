0a1519a680ec02953af8fbb53018a61dea10d36b
Ryan Williams <ryan.williams926@icloud.com>
2023-11-08 18:33:59 -0500

    Cutting down unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import CustomNavbar from './CustomNavbar';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 function App() {
 	return (
@@ -9,9 +12,12 @@ function App() {
 				<CustomNavbar></CustomNavbar>
 			</header>
 			<main>
-
-
-
+				<Container>
+					<Row className="px-4 my-5">
+						<Col sm={7}>sm=8</Col>
+						<Col sm={5}>sm=4</Col>
+					</Row>
+				</Container>
 			</main>
 		</div>
 	);
