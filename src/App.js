0832937f4fc633ff0e05b951aba573290c118264
Ryan Williams <ryan.williams926@icloud.com>
2023-11-08 18:41:56 -0500

    introducing side nav, updated file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import CustomNavbar from './CustomNavbar';
+import CustomNavbar from './components/CustomNavbar';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import StackedExample from './components/StackedExample';
 
 function App() {
 	return (
@@ -14,7 +15,10 @@ function App() {
 			<main>
 				<Container>
 					<Row className="px-4 my-5">
-						<Col sm={7}>sm=8</Col>
+						<Col sm={7}>sm=8
+							<StackedExample></StackedExample>
+						
+						</Col>
 						<Col sm={5}>sm=4</Col>
 					</Row>
 				</Container>
--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -7,7 +7,7 @@ function CustomNavbar() {
 	return (
 		<Navbar bg="dark" variant="dark" expand="lg">
 			<Container>
-				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				<Navbar.Brand href="#home">Canvas</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
