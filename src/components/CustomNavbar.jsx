064b587b0c5694f5c22f4d9b99b923b3c927e385
Ryan Williams <ryan.williams926@icloud.com>
2023-11-08 19:14:37 -0500

    red navbar

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,18 +1,19 @@
 import Container from 'react-bootstrap/Container';
+import styles from "../styles.module.css"
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function CustomNavbar() {
 	return (
-		<Navbar bg="dark" variant="dark" expand="lg">
+		<Navbar className={styles.navbarCustom} expand="lg">
 			<Container>
 				<Navbar.Brand href="#home">Canvas</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#link">Link</Nav.Link>
+						<Nav.Link className={styles.navbarCustom} href="#home">Home</Nav.Link>
+						<Nav.Link className={styles.navbarCustom} href="#link">Link</Nav.Link>
 						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
 							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
 							<NavDropdown.Item href="#action/3.2">
